cd9981bb257bb88ca37426c2b3639899f43e8614
liuyuan <liuyuan86@foxmail.com>
2022-10-13 15:58:52 +0800

    hot fix 03

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("this is hot fix");
         System.out.println("this is hot fix");
+        System.out.println("this is hot fix");
+
     }
 }
