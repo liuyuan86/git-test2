1305fee6842e622618114d3b444fac74a0fa75aa
liuyuan <liuyuan86@foxmail.com>
2022-10-13 15:56:15 +0800

    my fix 01

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -2,7 +2,6 @@ package com.atguigu;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello1");
-        System.out.println("hello2");
+        System.out.println("this is my fix");
     }
 }
