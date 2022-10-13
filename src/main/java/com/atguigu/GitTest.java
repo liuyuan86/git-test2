30cbd67b3a49a3e22d18cba9d338af8c8fe94891
liuyuan <liuyuan86@foxmail.com>
2022-10-13 16:09:29 +0800

    my fix 03

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("this is my fix");
         System.out.println("this is my fix");
+        System.out.println("this is my fix");
     }
 }
